d73dfc160f5d79c32995e49abfc4e404cc85de22
shkamath <shkamath@icloud.com>
2021-12-05 23:25:36 -0800

    highscore improvements

--- data/score.py
+++ data/score.py
@@ -1,6 +1,7 @@
 from turtle import Turtle
 FONT = ('Courier',20,'normal')
 ALIGNMENT = "center"
+SCORE_FILE_PATH="./data/high_score_data.txt"
 
 class ScoreBoard(Turtle):
    def __init__(self):
@@ -11,7 +12,7 @@ class ScoreBoard(Turtle):
       self.goto(0,275)
       self.hideturtle()
       self.score = 0
-      self.highscore = 0
+      self.highscore = self.get_high_score_record()
       self.write(f"Score: {self.score} High Score: {self.highscore}", align=ALIGNMENT, font= FONT)
 
    def update_score(self):
@@ -28,3 +29,19 @@ class ScoreBoard(Turtle):
          self.highscore = self.score
       self.score = 0
       self.update_score()
+      self.create_high_score_record()
+
+   def create_high_score_record(self):
+      with open(SCORE_FILE_PATH,mode="w") as file:
+         file.write(f"{self.highscore}")
+   
+   def get_high_score_record(self):
+      score = 0
+      try:
+         with open(SCORE_FILE_PATH,mode="r") as file:
+            score = int(file.read())
+      except:
+         print("File for high score does not exist. Creating one now")
+         with open(SCORE_FILE_PATH,mode="w") as file:
+            pass
+      return score
